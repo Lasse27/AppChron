41b4bea8322811ec3c1d4e3d35b3adcb7c70a336
Lasse27 <llhillen1403@gmail.com>
2025-02-23 22:28:56 +0100

    Add: Implemented the basic functionality for displaying database values on the ui

--- appchron/app.py
+++ appchron/app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 from flaskwebgui import FlaskUI
+from db_handler import DatabaseHandler
+import os
 
+
+
+# Constant
+_DATA_DIR: str = os.path.join(os.path.dirname(__file__), 'data')
+_SQLITE_PATH: str = _DATA_DIR + "/appchron.sqlite3"
 _APP: Flask = Flask(__name__)
+
 
 
 @_APP.route("/")
 def index():
     return render_template("index.html")
+
+
+
+# Route für Datenabfrage (wird via AJAX aufgerufen)
+@_APP.route('/refresh-data')
+def refresh_data():
+    handler : DatabaseHandler = DatabaseHandler(_SQLITE_PATH)
+    handler.connect()
+    query_result = handler.runQuery('Select * FROM PROCESS')
+    handler.disconnect()
+    
+    # Daten in JSON-Format für D3.js konvertieren
+    return jsonify([{'id': row[1], 'name': row[0]} for row in query_result])
+
 
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Main-Process >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> #
@@ -18,3 +40,6 @@ def runGUI() -> None:
     FlaskUI(
         app=_APP, port=80, fullscreen=False, width=800, height=600, server="flask"
     ).run()
+
+if __name__ == "__main__":
+    runGUI()
