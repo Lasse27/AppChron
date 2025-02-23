42b8fa21d38dda4de4c07a48ab0809ce02c963ca
Lasse27 <llhillen1403@gmail.com>
2025-02-23 15:12:43 +0100

    Change: Changed code formatting for better readability in watcher.py

--- appchron/watcher.py
+++ appchron/watcher.py
@@ -8,13 +8,18 @@ import threading
 
 class WatcherThread(threading.Thread):
 
+    #
+    #
+
     def __init__(self):
         super(WatcherThread, self).__init__()
 
+    #
+    #
+
     def run(self) -> None:
         """
-        ### Summary
-            Runs on start of watcher thread, general procedure.
+        Runs on start of watcher thread, general procedure.
         """
         durationSeconds: int = 0
         process: psutil.Process = self.getActiveProcess()
@@ -28,23 +33,28 @@ class WatcherThread(threading.Thread):
                 self.on_active_changed(process)
                 process = nextProcess
 
+    #
+    #
+
     def on_active_changed(self, process: psutil.Process) -> None:
         """
-        ### Summary:
-            Ereignis bei Wechsel des aktiven Prozesses
+        Ereignis bei Wechsel des aktiven Prozesses
 
-        ### Args:
+        *Args*:
             process (psutil.Process): Der Prozess der vorher aktiv war und nun geloggt wird.
         """
         print(process.pid)
         print(process.name())
         pass
 
+    #
+    #
+
     def getActiveProcess(self) -> psutil.Process:
         """
-        ### Summary:
-            Collects the currently active process from the win32 api.
-        ### Returns:
+        Collects the currently active process from the win32 api.
+
+        *Returns*:
             psutil.Process: The currently active process as psutil.Process instance.
         """
         windowHandle = win32gui.GetForegroundWindow()
